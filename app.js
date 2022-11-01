757580c07478ac9f10aa7f1ed7172670b4ef8da2
Ikki <sediqabdullahi04@gmail.com>
2022-11-01 12:49:05 +0100

    test deploy

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
-// const dotenv = require("dotenv").config();
-// const router = require("./modules/routes");
-// const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const router = require("./modules/routes");
+const mongoose = require("mongoose");
 // Mongodb connection
-// const uri = process.env.URI;
-// mongoose
-//   .connect(uri)
-//   .then((data) => {
-//     app.listen(process.env.PORT || 5000, console.log("Server running"));
-//   })
-//   .catch((err) => {
-//     app.listen(process.env.PORT, console.log("Error connecting to database"));
-//     console.log(err);
-//   });
+const uri = process.env.URI;
+mongoose
+  .connect(uri)
+  .then((data) => {
+    app.listen(process.env.PORT || 5000, console.log("Server running"));
+  })
+  .catch((err) => {
+    app.listen(process.env.PORT, console.log("Error connecting to database"));
+    console.log(err);
+  });
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-app.listen(process.env.PORT);
